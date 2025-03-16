20a2790da0dc3b95fa298523135e9e36a85ac4b3
Khushal Savalakha <khushal.sav@gmail.com>
2025-03-16 13:32:48 +0530

    Add home URL for cURL verification in hosted environment

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse  # Add this line
+
+def home(request):
+    return HttpResponse("Welcome to CineView Backend")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("booking/", include("BookingDetails.urls")),
     path("seats/", include("SeatDetails.urls")),
-    path("stripe/", include("stripe_payment.urls")),  # Add this line
+    path("stripe/", include("stripe_payment.urls")),
+    path("", home),
 ]
